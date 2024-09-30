f150f77e38e08de6bd6c36bb42eb5a3a6e3fcf9e
developer-felipe-andrade <felipe.andrade.developer@gmail.com>
2024-09-30 20:52:45 -0300

    iniciado o projeto front/back-end

---
+++ back-end/src/main/java/br/edu/ifpr/irati/ads/agenda_inteligente/AgendaInteligenteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.irati.ads.agenda_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaInteligenteApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/br/edu/ifpr/irati/ads/agenda_inteligente/AgendaInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.irati.ads.agenda_inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
