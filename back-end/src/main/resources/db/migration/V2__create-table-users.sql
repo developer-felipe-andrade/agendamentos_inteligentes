0a7e4da50b2dff54994ae04a239eeae951075014
developer-felipe-andrade <felipe.andrade.developer@gmail.com>
2024-11-11 19:54:53 -0300

    adicionado o docker compose do banco e documentação para interação das primeiras apis

--- back-end/src/main/resources/db/migration/V2__create-table-users.sql
+++ back-end/src/main/resources/db/migration/V2__create-table-users.sql
@@ -5,3 +5,12 @@ CREATE TABLE users (
     role TEXT NOT NULL,
     enabled BOOLEAN DEFAULT FALSE
 );
+
+INSERT INTO users (id, login, password, role, enabled)
+VALUES (
+  '12345678-abcd-1234-efgh-1234567890ab',
+  'admin@admin.com',
+  '$2a$10$gazPrnHS20O4CSZBkfgX4ul9pblrpgfSmnG0jFV7A.bDi16j9XfjS',
+  '0',
+  true
+);
